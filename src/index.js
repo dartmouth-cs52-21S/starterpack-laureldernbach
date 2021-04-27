8f4107c5951a6ec7d4b21c580a84e02960666af8
Laurel Dernbach <laurel.k.dernbach.22@dartmouth.edu>
2021-04-27 14:11:25 -0400

    routing woo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.scss';
+// eslint-disable-next-line object-curly-newline
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+
+const App = (props) => {
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+          <Route exact path="/test/:id" component={Test} />
+          <Route component={FallBack} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
