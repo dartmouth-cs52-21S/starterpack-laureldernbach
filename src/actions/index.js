17cbb29bca6de611d7532443eb40ad839d700205
isaacspokes <isaac.m.spokes.22@dartmouth.edu>
2021-05-19 12:12:00 -0400

    adding in new fetch actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,48 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://localhost:9090/api';
+
+const API_KEY = '?key=isaac_spokes';
+
 // keys for actiontypes
 export const ActionTypes = {
+  FETCH_SONGS: 'FETCH_SONGS',
+  FETCH_SONG: 'FETCH_SONG',
   INCREMENT: 'INCREMENT',
   DECREMENT: 'DECREMENT',
 };
+
+export function fetchSongs() {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/songs${API_KEY}`)
+      .then((response) => {
+        dispatch({
+          type: ActionTypes.FETCH_SONGS,
+          payload: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
+
+export function fetchSong(id) {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/songs/${id}${API_KEY}`)
+      .then((response) => {
+        dispatch({
+          type: ActionTypes.FETCH_SONG,
+          payload: response.data[0],
+        });
+        console.log('LOGGING SONG AT FETCH SONG');
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
 
 export function increment() {
   return {
